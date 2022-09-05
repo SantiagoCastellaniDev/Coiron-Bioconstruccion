b1d745b1e2c9e7630d4c8c95c4172a7890956b70
SantiagoCastellaniDev <santiagocastellani.dev@gmail.com>
2022-09-05 14:47:21 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './components/nav/nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SomosComponent } from './components/somos/somos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { RecursosComponent } from './components/recursos/recursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BannerComponent,
+    SomosComponent,
+    ServiciosComponent,
+    ProyectosComponent,
+    GaleriaComponent,
+    FooterComponent,
+    ContactoComponent,
+    RecursosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
